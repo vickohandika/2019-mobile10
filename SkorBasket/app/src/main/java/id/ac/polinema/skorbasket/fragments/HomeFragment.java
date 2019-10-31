3091e7ba7b9855930a883e4331714b51b5094881
vickohandika <vickonan8@gmail.com>
2019-10-31 15:10:20 +0700

    btnReset.setOnClickListener(new View.OnClickListener() {     @Override     public void onClick(View view) {         sharedScore.setScoreVisitor(0);         sharedScore.setScoreHome(0);     } });

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/HomeFragment.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/HomeFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,6 +37,8 @@ public class HomeFragment extends Fragment {
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//Instansiasi ViewModel SharedScore
+		sharedScore = ViewModelProviders.of(requireActivity()).get(SharedScore.class);
 	}
 
 	@Override
@@ -53,5 +57,33 @@ public class HomeFragment extends Fragment {
 		scoreSatuHome = view.findViewById(R.id.scoreSatuHome);
 
 		// Tambahkan logic tombol di bagian bawah ini
+		sharedScore.getScoreHome().observe(requireActivity(), new Observer<Integer>() {
+			@Override
+			public void onChanged(Integer score) {
+				scoreHome.setText(String.valueOf(score));
+				scoreDefault = score;
+			}
+		});
+		//Observer Pattern memerlukan Publisher.
+		scoreDuaHome.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreHome(scoreDefault + 2);
+			}
+		});
+
+		scoreTigaHome.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreHome(scoreDefault + 3);
+			}
+		});
+
+		scoreSatuHome.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreHome(scoreDefault + 1);
+			}
+		});
 	}
 }
